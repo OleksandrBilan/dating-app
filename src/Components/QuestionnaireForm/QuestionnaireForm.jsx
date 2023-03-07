0214ece1f2dba6bdce950eddde7c97a45fd918bd
Oleksandr Bilan <oleksandrbilan282002@gmail.com>
2023-03-07 16:30:29 +0200

    style QuestionnaireForm and add icons

--- src/Components/QuestionnaireForm/QuestionnaireForm.jsx
+++ src/Components/QuestionnaireForm/QuestionnaireForm.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import s from "./style.module.css";
 import { API_URL } from "../../config";
+import { PatchCheck, XCircle } from "react-bootstrap-icons";
 
 export function QuestionnaireForm() {
   const [questionnaire, setQuestionnaire] = useState([]);
@@ -21,25 +22,40 @@ export function QuestionnaireForm() {
 
   function ShowQuestion(question) {
     return (
-      <div className={s.question} key={question.id}>
-        <input
-          type="text"
-          name={question.id}
-          className="form-control"
-          onChange={updateQuestionName}
-          defaultValue={question.name}
-          readOnly={true}
-        />
+      <div className={s.section}>
+        <div className={s.question} key={question.id}>
+          <input
+            type="text"
+            name={question.id}
+            className={`form-control ${s.question_name}`}
+            onChange={updateQuestionName}
+            defaultValue={question.name}
+          />
+          <div className={s.buttons}>
+            <div className={s.tooltip}>
+              <span class={s.tooltiptext}>Save question with answers</span>
+              <PatchCheck fill="green" className={s.save_icon} />
+            </div>
+            <div className={s.tooltip}>
+              <span class={s.tooltiptext}>Delete question with answers</span>
+              <XCircle fill="red" className={s.delete_icon} />
+            </div>
+          </div>
+        </div>
         <div className={s.answers}>
           {question.answers.map((a) => (
-            <input
-              type="text"
-              name={a}
-              className="form-control"
-              onChange={updateAnswer}
-              defaultValue={a}
-              readOnly={true}
-            />
+            <div className={s.answer}>
+              <input
+                type="text"
+                name={a}
+                className={`form-control ${s.answer_name}`}
+                onChange={updateAnswer}
+                defaultValue={a}
+              />
+              <div className={s.buttons}>
+                <XCircle fill="red" className={s.delete_icon} />
+              </div>
+            </div>
           ))}
         </div>
       </div>
