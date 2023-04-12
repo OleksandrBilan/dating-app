525678e4dba255a586a0996b8e07f7e7ee2ffcac
Oleksandr Bilan <oleksandrbilan282002@gmail.com>
2023-04-12 16:20:33 +0300

    add image change

---
+++ src/Components/ImageUploadForm/ImageUploadForm.jsx
@@ -0,0 +1,73 @@
+import { useEffect, useState } from "react";
+import s from "./style.module.css";
+import { Button } from "react-bootstrap";
+
+export function ImageUploadForm({ previewImageUrl, onSubmit }) {
+  const [image, setImage] = useState();
+  const [previewUrl, setPreviewUrl] = useState();
+
+  useEffect(() => {
+    setPreviewUrl(previewImageUrl);
+  }, [previewImageUrl]);
+
+  function onFileSelect(e) {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onloadend = () => {
+        setImage(file);
+        setPreviewUrl(reader.result);
+      };
+    }
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (image) {
+      onSubmit(image);
+    } else {
+      alert("No files selected");
+    }
+  }
+
+  return (
+    <div className={s.container}>
+      <form onSubmit={handleSubmit} className={s.imageUpload}>
+        <div>
+          {previewUrl && (
+            <img
+              src={previewUrl}
+              style={{ maxWidth: "100%", maxHeight: "300px" }}
+              alt="Selected image"
+            />
+          )}
+        </div>
+        <div>
+          <label className={s.imageUploadLabel}>
+            {image ? (
+              <span className={s.imageUploadFilename}>{image.name}</span>
+            ) : (
+              <span>Choose an image</span>
+            )}
+            <input
+              type="file"
+              name="image"
+              className={s.imageUploadInput}
+              onChange={onFileSelect}
+            />
+          </label>
+        </div>
+        <div>
+          <Button
+            type="submit"
+            variant="primary"
+            className={s.ImageUploadButton}
+          >
+            Upload
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+}
--- src/Pages/EditUserProfile/EditUserProfile.jsx
+++ src/Pages/EditUserProfile/EditUserProfile.jsx
@@ -5,12 +5,26 @@ import { AuthService } from "../../Services/auth";
 import axios from "axios";
 import { API_URL } from "../../config";
 import { useEffect, useState } from "react";
+import { ImageUploadForm } from "../../Components/ImageUploadForm/ImageUploadForm";
+import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 export function EditUserProfile() {
+  const navigate = useNavigate();
   const [userInfo, setUserInfo] = useState();
+  const [userImageUrl, setUserImageUrl] = useState();
 
   useEffect(() => {
-    setUserInfo(AuthService.getUserInfo());
+    const user = AuthService.getUserInfo();
+    setUserInfo(user);
+    axios
+      .get(`${API_URL}/user/getImage?userId=${user.id}`, {
+        responseType: "blob",
+      })
+      .then((response) => {
+        const url = URL.createObjectURL(response.data);
+        setUserImageUrl(url);
+      });
   }, []);
 
   function onFormSubmit(formValues) {
@@ -35,11 +49,66 @@ export function EditUserProfile() {
       });
   }
 
+  function onImageUpload(file) {
+    const formData = new FormData();
+    formData.append("image", file);
+    axios({
+      method: "post",
+      url: `${API_URL}/user/uploadImage?userId=${userInfo.id}`,
+      data: formData,
+      headers: { "Content-Type": "multipart/form-data" },
+    })
+      .then((response) => alert("Image uploaded successfully :)"))
+      .catch((error) => alert("Error uploading the image :("));
+  }
+
+  function onAccountDelete() {
+    if (window.confirm("Are you sure you want to delete your account?")) {
+      axios
+        .delete(`${API_URL}/user/delete?id=${userInfo.id}`)
+        .then((response) => {
+          AuthService.deleteAuthTokenCookie();
+          AuthService.removeAuthTokenFromAxios();
+          navigate("/login");
+        })
+        .catch((error) => {
+          alert("Can't delete the account :(");
+        });
+    }
+  }
+
+  function onUserImageDelete() {
+    if (window.confirm("Are you sure you want to delete your account image?")) {
+      axios
+        .delete(`${API_URL}/user/deleteImage?userId=${userInfo.id}`)
+        .then((response) => {
+          setUserImageUrl("");
+        })
+        .catch((error) => {
+          alert("Can't delete the image :(");
+        });
+    }
+  }
+
   return (
     <div className={s.container}>
       <CustomNavbar />
       <div className={s.card}>
+        <div className={s.changeImageDiv}>
+          <ImageUploadForm
+            onSubmit={onImageUpload}
+            previewImageUrl={userImageUrl}
+          />
+          <Button variant="danger" onClick={onUserImageDelete}>
+            Delete profile image
+          </Button>
+        </div>
         <UserInfoForm onSubmit={onFormSubmit} userInfo={userInfo} />
+        <div className={s.deleteButtonDiv}>
+          <Button variant="danger" onClick={onAccountDelete}>
+            Delete account
+          </Button>
+        </div>
       </div>
     </div>
   );
